e9b9ce1c4a80b10d25351b2ac83ae24671d4be61
Pritesh Rathod <rathodpritesh0712@gmail.com>
2026-07-12 19:21:00 +0530

    integration of routes on backend

--- zentriom-backend/app/main.py
+++ zentriom-backend/app/main.py
@@ -5,9 +5,14 @@ from app.api.auth import router as auth_router
 from app.api.chat import router as chat_router
 from app.api.grammer import router as grammer_router
 from app.api.linkedin import router as linkedin_router
+from app.api.code_explainer import router as code_explainer_router
+
+from app.api.history import router as history_router
 
 from app.db.database import engine, Base
 from app.models.users import User
+from app.models.password_reset_otp import PasswordResetOTP
+from app.models.ai_history import AIHistory
 
 app = FastAPI()
 
@@ -19,13 +24,14 @@ app.add_middleware(
     allow_credentials=True,
 )
 
-User.metadata.create_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
 app.include_router(chat_router)
 app.include_router(grammer_router)
 app.include_router(linkedin_router)
+app.include_router(code_explainer_router)
+app.include_router(history_router)
 
 @app.get("/")
 def root():
